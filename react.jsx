48128e367d48679117c2d6c7f05334b9dc0701b7
navyakp <navyakpkp@gmail.com>
2026-06-25 14:43:23 +0530

    new file  react.jsx added

---
+++ react.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+      <p>This is my first React application.</p>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
